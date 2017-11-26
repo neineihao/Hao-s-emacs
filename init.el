255657c1df5f392a54da18ac41a02ef8ef37d60e
Chen's Mac <s894117@gmail.com>
2017-11-26 22:26:59 +0900

    add some setting

--- init.el
+++ init.el
@@ -18,10 +18,55 @@
 (use-package try
   :ensure t)
 
-
+;;which key package to support M-x
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+;;Org mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;;Hungry delete
+(use-package hungry-delete
+  :ensure t
+  :config (require 'hungry-delete))
+
+;;Smart parenthesis
+(use-package smartparens
+  :ensure t
+  :config (require 'smartparens-config))
+(smartparens-global-mode t)
+
+
+;;Monokai-theme
+(use-package monokai-theme
+  :ensure t)
+
+;;Some emacs setting
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-linum-mode t)
+(global-hl-line-mode t)
+(set-face-attribute 'default nil :height 140)
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;;Setting shortcut for init.el
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+;;Enable recent file
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 
 
@@ -31,7 +76,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key try use-package))))
+ '(package-selected-packages
+   (quote
+    (monokai-theme smartparens hungry-delete org-bullets which-key try use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
